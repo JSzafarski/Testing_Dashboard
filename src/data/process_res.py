83da765a5bd38fd7ccb3a8ef5191fd90ca1bab54
j szafarski <js1200@exeter.ac.uk>
2025-06-09 15:36:36 +0200

    added a basic result procesing script

---
+++ src/data/process_res.py
@@ -0,0 +1,28 @@
+import os
+
+import pandas as pd
+import sqlite3
+
+
+def save_results_to_csv():
+    """
+
+    :return:
+    """
+    # Connect to database
+    connection = sqlite3.connect("example_results.db")
+    df = pd.read_sql_query('SELECT * FROM results', connection)
+    connection.close() #close the database after we finish the read operation
+
+    # Process and save to CSV
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df.to_csv("src/data/test_results.csv", index=False)
+    print("Results saved to src/data/test_results.csv")
+    #effecitvely returns a list of dictionaries where each dictionary represents a row from the results table
+    #that is present in the database
+    return df.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    results = save_results_to_csv()
+    print(f"Processed Results: {results}")
